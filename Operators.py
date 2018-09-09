42ee448a4fe5b2ea1f564285dd92cdb1bca95696
Jason Wu <lujason2015@gmail.com>
2018-09-09 12:17:51 -0400

    Update Operators.py

--- Operators.py
+++ Operators.py
@@ -1,4 +1,16 @@
 #!/bin/python3
+"""
+Objective 
+In this challenge, you'll work with arithmetic operators. 
+Check out the Tutorial tab for learning materials and an 
+instructional video!
+
+Task 
+Given the meal price (base cost of a meal), tip percent
+(the percentage of the meal price being added as tip), 
+and tax percent (the percentage of the meal price being
+added as tax) for a meal, find and print the meal's total cost.
+"""
 
 import math
 import os
@@ -8,10 +20,11 @@ import sys
 
 # Complete the solve function below.
 def solve(meal_cost, tip_percent, tax_percent):
-    tip = meal_cost * float(tip_percent)/100
-    tax = meal_cost * float(tax_percent)/100
-    totalCost = meal_cost + tip + tax 
-    return round(totalCost)
+    tip = meal_cost * tip_percent/100
+    tax = meal_cost * tax_percent/100
+    totalCost = round(meal_cost + tip + tax)
+    print(totalCost)
+    return totalCost
 
 if __name__ == '__main__':
     meal_cost = float(input())

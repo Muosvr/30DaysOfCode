4ed3f2de20f2bb277bf938368b04f405f23d72e7
Muosvr <lujason2015@gmail.com>
2018-09-17 10:37:14 -0400

    BinaryNumbers.py

---
+++ BinaryNumbers.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    rev_bin = ""
+    one_arr = []
+    max_one = 0
+
+    while n > 0:
+        remainder = n%2
+        n = n // 2
+        rev_bin += str(remainder)
+
+    one_arr = rev_bin.split('0')
+
+    for i in one_arr:
+        if max_one < len(i):
+            max_one = len(i)
+
+    print(max_one)

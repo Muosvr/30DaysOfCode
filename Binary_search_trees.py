aa0c830df9b3df92eb581421cf638b965840786f
Jason Wu <lujason2015@gmail.com>
2018-10-11 00:00:11 -0400

    Update Binary_search_trees.py

--- Binary_search_trees.py
+++ Binary_search_trees.py
@@ -15,9 +15,13 @@ class Solution:
                 root.right=cur
         return root
     
-        def getHeight(self,root):
+    def getHeight(self,root):
         #Write your code here
-        
+        if root == None:
+            return -1
+        else:
+            return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
 T=int(input())
 myTree=Solution()
 root=None

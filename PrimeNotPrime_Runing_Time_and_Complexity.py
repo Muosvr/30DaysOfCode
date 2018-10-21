52399c919c5f2eb1bf3273788da020e417f88f25
Muosvr <lujason2015@gmail.com>
2018-10-21 15:30:30 -0400

    prime not prime

--- PrimeNotPrime_Runing_Time_and_Complexity.py
+++ PrimeNotPrime_Runing_Time_and_Complexity.py
@@ -4,10 +4,13 @@ inputs=[]
 for i in range(n):
     number = int(input())
     prime = True
-    for i in range(2, number):
-        if number%i==0:
-            prime = False
-    if prime:
-        print("Prime")
-    else:
+    if number == 1:
         print("Not prime")
+    else:
+        for i in range(2, int(number**0.5)+1):
+            if number%i==0:
+                prime = False
+        if prime:
+            print("Prime")
+        else:
+            print("Not prime")

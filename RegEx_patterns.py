d35d157af92ad41953e81fb3d7083617bfb7266f
Muosvr <lujason2015@gmail.com>
2018-11-12 20:02:26 -0500

    order

--- RegEx_patterns.py
+++ RegEx_patterns.py
@@ -10,10 +10,21 @@ import sys
 
 if __name__ == '__main__':
     N = int(input())
+    list = []
 
     for N_itr in range(N):
-        firstNameEmailID = input().split()
+        firstNameEmailID = input().split(" ")
 
         firstName = firstNameEmailID[0]
 
         emailID = firstNameEmailID[1]
+        index = 0
+        for name in list:
+            if ord(name[0]) < ord(firstName[0]):
+                print(ord(name[0]), ord(firstName[0]))
+                index += 1
+        list.insert(index,firstName)
+
+    # for name in list:
+    #     print(name)
+    print(list)

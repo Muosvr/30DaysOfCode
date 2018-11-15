41642ffce3b4d43f1104164a368e4e61010c45cd
Muosvr <lujason2015@gmail.com>
2018-11-15 18:15:10 -0500

    complete

--- RegEx_patterns.py
+++ RegEx_patterns.py
@@ -12,19 +12,24 @@ if __name__ == '__main__':
     N = int(input())
     list = []
 
+    pattern = r'@gmail.com$';
+
     for N_itr in range(N):
         firstNameEmailID = input().split(" ")
 
         firstName = firstNameEmailID[0]
 
         emailID = firstNameEmailID[1]
-        index = 0
-        for name in list:
-            if ord(name[0]) < ord(firstName[0]):
-                print(ord(name[0]), ord(firstName[0]))
-                index += 1
-        list.insert(index,firstName)
+        if re.search(pattern, emailID, re.M):
+            # print("Found email!")
+            index = 0
+            for name in list:
+                if ord(name[0]) < ord(firstName[0]):
+                    # print(ord(name[0]), ord(firstName[0]))
+                    index += 1
+            list.insert(index,firstName)
 
     # for name in list:
     #     print(name)
-    print(list)
+    for name in list:
+        print(name)
